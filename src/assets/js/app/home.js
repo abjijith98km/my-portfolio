dfb219f83e78c842ceaabf949cd9cbc37dc829a3
abhijith <abhijith@pixelflames.com>
2023-06-23 17:54:22 +0530

    homepage development

---
+++ src/assets/js/app/home.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  // const cursor = new MouseFollower({
+  //   el: null,
+  //   container: document.body,
+  //   className: "cursor",
+  //   innerClassName: "mf-cursor-inner",
+  //   textClassName: "mf-cursor-text",
+  //   mediaClassName: "mf-cursor-media",
+  //   mediaBoxClassName: "mf-cursor-media-box",
+  //   iconSvgClassName: "mf-svgsprite",
+  //   iconSvgNamePrefix: "-",
+  //   iconSvgSrc: "",
+  //   dataAttr: "cursor",
+  //   hiddenState: "-hidden",
+  //   textState: "-text",
+  //   iconState: "-icon",
+  //   activeState: "-active",
+  //   mediaState: "-media",
+  //   stateDetection: {
+  //     "-pointer": "a,button",
+  //     "-hidden": "iframe",
+  //   },
+  //   visible: true,
+  //   visibleOnState: false,
+  //   speed: 0.55,
+  //   ease: "expo.out",
+  //   overwrite: true,
+  //   skewing: 0,
+  //   skewingText: 2,
+  //   skewingIcon: 2,
+  //   skewingMedia: 2,
+  //   skewingDelta: 0.001,
+  //   skewingDeltaMax: 0.15,
+  //   stickDelta: 0.15,
+  //   showTimeout: 20,
+  //   hideOnLeave: false,
+  //   hideTimeout: 300,
+  //   hideMediaTimeout: 300,
+  // });
+
+  // const el = document.querySelector(".portfolio_listing_card");
+
+  // el.addEventListener("mouseenter", () => {
+  //   cursor.setImg("/uploads/home/arrow-right.svg");
+  // });
+});
--- src/assets/js/plugins.js
+++ src/assets/js/plugins.js
@@ -1,2 +1,4 @@
+//=require plugins/gsap.js
 //=require plugins/bootstrap.min.js
 //=require plugins/slick.min.js
+//=require plugins/cubrto.js
